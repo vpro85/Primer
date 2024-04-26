586f1a3bb4ce9ebc9fbd15d631946f356d592e64
Vadim Prokoshin <vpro85@gmail.com>
2024-04-26 23:33:37 +0300

    3.19. Addressing the ambiguity in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 function myFunction(param: number | string) {
   if (typeof param == 'number' || typeof param == 'string') {
-    let result = param + 100;
+    let result = (param as any) + 100;
     console.log('My result: ' + result);
   } else {
     throw 'Expected a number or a string: ' + param;
