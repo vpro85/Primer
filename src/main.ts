0c14ff73ec9e5e7d36f340065af2fd6db0831b1d
Vadim Prokoshin <vpro85@gmail.com>
2024-04-27 00:32:45 +0300

    3.33. Using the equality operator in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -61,3 +61,12 @@ switch (firstName) {
     console.log('firstName is neither Adam or Jacqui');
     break;
 }
+
+let firstVal: any = 5;
+let secondVal: any = '5';
+
+if (firstVal == secondVal) {
+  console.log('They are the same');
+} else {
+  console.log('They are NOT the same');
+}
