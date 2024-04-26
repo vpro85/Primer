868a97f6a7d90b237e1eac698aaf580f97e40c58
Vadim Prokoshin <vpro85@gmail.com>
2024-04-26 23:40:07 +0300

    3.20. Accessing a type feature in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 function myFunction(param: number | string) {
   if (typeof param == 'number' || typeof param == 'string') {
-    let result = (param as any) + 100;
-    console.log('My result: ' + result);
+    let fixed = param.toFixed(2);
+    console.log('My result: ' + fixed);
   } else {
     throw 'Expected a number or a string: ' + param;
   }
