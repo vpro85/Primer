6332192865f4b8aafbfb5bf4079b2640cc540df5
Vadim Prokoshin <vpro85@gmail.com>
2024-04-27 00:15:53 +0300

    3.29. Using a template string in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,19 @@ let person: string = 'Bob';
 const age: number = 40;
 
 let place: string | undefined | null;
-console.log('Place value: ' + place + ' Type: ' + typeof place);
+// console.log('Place value: ' + place + ' Type: ' + typeof place);
+console.log(`Place value: ${place} Type: ${typeof place}`);
 
 place = 'London';
-console.log('Place value: ' + place + ' Type: ' + typeof place);
+// console.log('Place value: ' + place + ' Type: ' + typeof place);
+console.log(`Place value: ${place} Type: ${typeof place}`);
 
 place = null;
-console.log('Place value: ' + place + ' Type: ' + typeof place);
+// console.log('Place value: ' + place + ' Type: ' + typeof place);
+console.log(`Place value: ${place} Type: ${typeof place}`);
 
 let firstBool = true;
 let secondBool = false;
 
-let firstString = "This is a string";
+let firstString = 'This is a string';
 let secondString = 'And so is this';
