908cd72e35d49a1844e45a70257831d73f43129a
Vadim Prokoshin <vpro85@gmail.com>
2024-04-26 23:14:21 +0300

    3.13. Replacing the contents of the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-console.log("Hello")
-console.log("Hello, World")
+function myFunction(param) {
+  if (typeof param == 'number') {
+    let result = param + 100;
+    console.log('My result: ' + result);
+  } else {
+    throw 'Expected a number: ' + param;
+  }
+}
+
+myFunction(1);
+myFunction('London');
