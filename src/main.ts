c44a7e67b13fee339982728b346c1ddd7c50933e
Vadim Prokoshin <vpro85@gmail.com>
2024-04-26 23:57:53 +0300

    3.25. Assigning null in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,6 @@ console.log('Place value: ' + place + ' Type: ' + typeof place);
 
 place = 'London';
 console.log('Place value: ' + place + ' Type: ' + typeof place);
+
+place = null;
+console.log('Place value: ' + place + ' Type: ' + typeof place);
