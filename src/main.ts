6a5077ab0f77191b52cffe4562fe00b9ac3fae62
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 01:53:51 +0300

    3.41. Using the nullish-coalescing operator in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -87,9 +87,9 @@ let val1: string | undefined;
 let val2: string | undefined = 'London';
 let val3: number | undefined = 0;
 
-let coalesced1 = val1 || 'fallback value';
-let coalesced2 = val2 || 'fallback value';
-let coalesced3 = val3 || 100;
+let coalesced1 = val1 ?? 'fallback value';
+let coalesced2 = val2 ?? 'fallback value';
+let coalesced3 = val3 ?? 100;
 
 console.log(`Result 1: ${coalesced1}`);
 console.log(`Result 2: ${coalesced2}`);
