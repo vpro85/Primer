fc2f776911a8da0209c65db33006db9d648ce3e2
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 01:41:34 +0300

    3.40. An unexpected null coalescing result in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -85,9 +85,12 @@ console.log(`Result 2: ${myData2}, Type: ${typeof myData2}`);
 
 let val1: string | undefined;
 let val2: string | undefined = 'London';
+let val3: number | undefined = 0;
 
 let coalesced1 = val1 || 'fallback value';
 let coalesced2 = val2 || 'fallback value';
+let coalesced3 = val3 || 100;
 
 console.log(`Result 1: ${coalesced1}`);
 console.log(`Result 2: ${coalesced2}`);
+console.log(`Result 3: ${coalesced3}`);
