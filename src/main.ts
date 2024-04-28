2a4f8c994280928e2f945fff67970a4772b91db1
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 02:02:17 +0300

    3.43. Using the optional chaining operator in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -100,3 +100,6 @@ if (count != null && count !== undefined) {
   let result1: string = count.toFixed(2);
   console.log(`Result 1: ${result1}`);
 }
+
+let result2: string | undefined = count?.toFixed(2);
+console.log(`Result 2: ${result2}`);
