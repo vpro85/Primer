a310cbf4eab27023b48c3bb329a14450f50ab24c
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 00:36:20 +0300

    4.18. Processing a data array in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -131,6 +131,12 @@ writeCity(() => myCity);
 let myArray: (number | string | boolean)[] = [100, 'Adam', true];
 let otherArray = [...myArray, 200, 'Bob', false];
 
+let sum: number = otherArray
+  .filter((val) => typeof val == 'number')
+  .reduce((total: number, val) => total + (val as number), 0);
+
+console.log(`Sum: ${sum}`);
+
 // for (let i = 0; i < myArray.length; i++) {
 //   console.log('Index ' + i + ': ' + myArray[i]);
 // }
