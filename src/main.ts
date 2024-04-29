8cd8e5b75e216ababc098e5b8db6d3c8551b3fc0
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 01:39:51 +0300

    4.24. Defining a method in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -168,24 +168,20 @@ class Product {
   name: string;
   price: number;
   category?: string;
+
+  printDetails() {
+    if (this.category != undefined) {
+      console.log(
+        `Name: ${this.name}, Price: ${this.price}, Category: ${this.category}`
+      );
+    } else {
+      console.log(`Name: ${this.name}, Price: ${this.price}`);
+    }
+  }
 }
 
 let hat = new Product('Hat', 100);
 let boots = new Product('Boots', 100, 'Snow Gear');
 
-function printDetails(product: {
-  name: string;
-  price: number;
-  category?: string;
-}) {
-  if (product.category != undefined) {
-    console.log(
-      `Name: ${product.name}, Price: ${product.price}, Category: ${product.category}`
-    );
-  } else {
-    console.log(`Name: ${product.name}, Price: ${product.price}`);
-  }
-}
-
-printDetails(hat);
-printDetails(boots);
+hat.printDetails();
+boots.printDetails();
