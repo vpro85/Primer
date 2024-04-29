599c51ac740ca3e5e2ca4f0f417dbea79dafd8ec
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 11:11:37 +0300

    4.3. Defining an optional parameter in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -104,9 +104,9 @@ if (count != null && count !== undefined) {
 let result2: string | undefined = count?.toFixed(2);
 console.log(`Result 2: ${result2}`);
 
-function writeValue(val: string | null) {
+function writeValue(val?: string) {
   console.log(`Value: ${val ?? 'Fallback value'}`);
 }
 
 writeValue('London');
-writeValue(null);
+writeValue();
