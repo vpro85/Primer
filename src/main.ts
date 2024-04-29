8846c21118573674a31e6443e43ceb4bda347915
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 11:58:37 +0300

    4.16. Enumerating the contents of an array in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -130,5 +130,10 @@ writeCity(() => myCity);
 
 let myArray: (number | string | boolean)[] = [100, 'Adam', true];
 
-let val = myArray[0];
-console.log(`Value: ${val}`);
+for (let i = 0; i < myArray.length; i++) {
+  console.log('Index ' + i + ': ' + myArray[i]);
+}
+
+console.log('---');
+
+myArray.forEach((value, index) => console.log('Index ' + index + ': ' + value));
