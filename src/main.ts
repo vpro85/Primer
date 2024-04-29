9820e16cc84670653d930296a9167008d3675c8c
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 01:14:09 +0300

    4.20. Describing an object type in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -157,5 +157,9 @@ let boots = {
   price: 100,
 };
 
-console.log(`Name: ${hat.name}, Price: ${hat.price}`);
-console.log(`Name: ${boots.name}, Price: ${boots.price}`);
+function printDetails(product: { name: string; price: number }) {
+  console.log(`Name: ${product.name}, Price: ${product.price}`);
+}
+
+printDetails(hat);
+printDetails(boots);
