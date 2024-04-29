d6c1b282ac21c0d11660abe321ab359d1893f2b0
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 11:49:50 +0300

    4.13. Populating a new array in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -128,7 +128,4 @@ writeCity(() => 'Paris');
 let myCity = 'Rome';
 writeCity(() => myCity);
 
-let myArray: (number | string | boolean)[] = [];
-myArray[0] = 100;
-myArray[1] = 'Adam';
-myArray[2] = true;
+let myArray: (number | string | boolean)[] = [100, 'Adam', true];
