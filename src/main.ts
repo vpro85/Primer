437f1e6628bd65034fa781907ba77962987ca610
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 01:09:35 +0300

    4.19. Creating an object in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -146,3 +146,16 @@ console.log(`Sum: ${sum}`);
 otherArray.forEach((value, index) =>
   console.log('Index ' + index + ': ' + value)
 );
+
+let hat = {
+  name: 'Hat',
+  price: 100,
+};
+
+let boots = {
+  name: 'Boots',
+  price: 100,
+};
+
+console.log(`Name: ${hat.name}, Price: ${hat.price}`);
+console.log(`Name: ${boots.name}, Price: ${boots.price}`);
