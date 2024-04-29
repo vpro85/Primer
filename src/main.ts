15e920a26c15e04692be84c19b2550321f1d79cf
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 11:47:06 +0300

    4.12. Restricting array value types in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -128,7 +128,7 @@ writeCity(() => 'Paris');
 let myCity = 'Rome';
 writeCity(() => myCity);
 
-let myArray: any[] = [];
+let myArray: (number | string | boolean)[] = [];
 myArray[0] = 100;
 myArray[1] = 'Adam';
 myArray[2] = true;
