faf233b5273107a9a95bd047b6005a2cec646396
Vadim Prokoshin <vpro85@gmail.com>
2024-04-29 11:45:40 +0300

    4.11. Using a type annotation in the main.ts file in the src folder

--- src/main.ts
+++ src/main.ts
@@ -128,7 +128,7 @@ writeCity(() => 'Paris');
 let myCity = 'Rome';
 writeCity(() => myCity);
 
-let myArray = [];
+let myArray: any[] = [];
 myArray[0] = 100;
 myArray[1] = 'Adam';
 myArray[2] = true;
